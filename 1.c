8c3a6f65d4dc5ea60255426defb9fa16aa328247
ajinkyaedhate <91676116+ajinkyaedhate@users.noreply.github.com>
2023-09-16 12:56:32 +0530

    delete 1.c

--- 1.c
+++ 1.c
@@ -1,16 +1,1 @@
-// 1. Write a program to print unit digit of a given number
-#include <stdio.h>
 
-int main()
-{
-    int num;
-
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    int unitDigit = num % 10;
-
-    printf("The unit digit of %d is %d\n", num, unitDigit);
-
-    return 0;
-}
